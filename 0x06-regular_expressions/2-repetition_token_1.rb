0ba065bc1489d0d3f00de5dfaacf841cc33ec7fa
NO-MM <khuzwanohnoh@gmail.com>
2024-11-08 18:38:43 -0800

    Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# Ruby script that accepts one argument and pass it to a regular
-# expression matching method.
+# Ruby script that accepts one argument and pass it to
+#  a regular expression matching method.
 
-puts ARGV[0].scan(hbt?tn/).join
+puts ARGV[0].scan(/hbt?tn/).join
